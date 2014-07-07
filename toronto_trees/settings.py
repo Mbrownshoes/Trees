b246334703132b102162992924b922cf37f15566
Mathew Brown <mathew.brown@utoronto.ca>
2014-07-07 16:05:25 -0400

    oved static files, added accounts app

--- toronto_trees/settings.py
+++ toronto_trees/settings.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-# BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SETTINGS_DIR = os.path.dirname(__file__)
 PROJECT_PATH = os.path.join(SETTINGS_DIR, os.pardir)
 PROJECT_PATH = os.path.abspath(PROJECT_PATH)
@@ -121,9 +121,9 @@ USE_TZ = True
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, '../static')
 
-STATIC_PATH = os.path.join(PROJECT_PATH,'static')
+# STATIC_PATH = os.path.join(PROJECT_PATH,'static')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
@@ -131,11 +131,14 @@ STATIC_PATH = os.path.join(PROJECT_PATH,'static')
 STATIC_URL = '/static/'
 # Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    STATIC_PATH,
+    os.path.join(BASE_DIR, 'toronto_trees', 'static'),
 )
+# STATICFILES_DIRS = (
+#     # Put strings here, like "/home/html/static" or "C:/www/django/static".
+#     # Always use forward slashes, even on Windows.
+#     # Don't forget to use absolute paths, not relative paths.
+#     STATIC_PATH,
+# )
 
 # List of finder classes that know how to find static files in
 # various locations.
