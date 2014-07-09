25669a841d7b70b2e0bc7393d33ced19eb7fd48a
Mathew Brown <mathew.brown@utoronto.ca>
2014-07-09 09:52:34 -0400

    FT mostly passing. Am able to login but wait_to_be_logged_out is failing

--- treemap/urls.py
+++ treemap/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import patterns, url
-from treemap import views
 
 # urlpatterns =patterns('',
 #     url(r'^$',views.MapView.as_view()))
 
 urlpatterns =patterns('',
     # url(r'^$',views.index, name='index'),
-        (r'^$', views.map_page),
+    url(r'^$', 'treemap.views.map_page'),
     )
