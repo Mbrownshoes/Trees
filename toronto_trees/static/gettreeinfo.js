6e912044068a7e7223793cd57954815112348b42
Mathew Brown <mathew.brown@utoronto.ca>
2014-07-24 17:21:03 -0400

    added geolocation to gettreeinfo.js and added css and html button

--- toronto_trees/static/gettreeinfo.js
+++ toronto_trees/static/gettreeinfo.js
@@ -1,5 +1,11 @@
 
 var map, layer, info;
+var vector = new OpenLayers.Layer.Vector('vector');
+var style = {
+    fillColor: '#000',
+    fillOpacity: 0.1,
+    strokeWidth: 0
+};
 var apiKey = "AhjCmC4ThYrtk-3i-01G6buMsQBPOi0SfZO6CCOyhbd1XTHSkI1T_00AZiCBL1HV";
 function init(){
   var geographic = new OpenLayers.Projection("EPSG:4326");
@@ -94,7 +100,7 @@ OpenLayers.Control.ListenToClick = OpenLayers.Class(OpenLayers.Control, {
 
     if (url) {
       document.getElementById('info').innerHTML =
-      '<iframe seamless src="' + url + '" scrolling="no" align="middle"  frameBorder="0" height=200%></iframe>';
+      '<iframe seamless src="' + url + '" scrolling="no" align="middle"  frameBorder="0" height=100%></iframe>';
     }
     // url.activate();
   },
@@ -115,5 +121,98 @@ var ctmControl = new OpenLayers.Control.ListenToClick({
 map.addControl(ctmControl);
 ctmControl.activate();
 
+var pulsate = function(feature) {
+    var point = feature.geometry.getCentroid(),
+        bounds = feature.geometry.getBounds(),
+        radius = Math.abs((bounds.right - bounds.left)/2),
+        count = 0,
+        grow = 'up';
+
+    var resize = function(){
+        if (count>16) {
+            clearInterval(window.resizeInterval);
+        }
+        var interval = radius * 0.03;
+        var ratio = interval/radius;
+        switch(count) {
+            case 4:
+            case 12:
+                grow = 'down'; break;
+            case 8:
+                grow = 'up'; break;
+        }
+        if (grow!=='up') {
+            ratio = - Math.abs(ratio);
+        }
+        feature.geometry.resize(1+ratio, point);
+        vector.drawFeature(feature);
+        count++;
+    };
+    window.resizeInterval = window.setInterval(resize, 50, point, radius);
+};
+
+var geolocate = new OpenLayers.Control.Geolocate({
+    bind: false,
+    geolocationOptions: {
+        enableHighAccuracy: false,
+        maximumAge: 0,
+        timeout: 7000
+    }
+});
+map.addControl(geolocate);
+var firstGeolocation = true;
+geolocate.events.register("locationupdated",geolocate,function(e) {
+    vector.removeAllFeatures();
+    var circle = new OpenLayers.Feature.Vector(
+        OpenLayers.Geometry.Polygon.createRegularPolygon(
+            new OpenLayers.Geometry.Point(e.point.x, e.point.y),
+            e.position.coords.accuracy/2,
+            40,
+            0
+        ),
+        {},
+        style
+    );
+    vector.addFeatures([
+        new OpenLayers.Feature.Vector(
+            e.point,
+            {},
+            {
+                graphicName: 'cross',
+                strokeColor: '#f00',
+                strokeWidth: 2,
+                fillOpacity: 0,
+                pointRadius: 10
+            }
+        ),
+        circle
+    ]);
+    if (firstGeolocation) {
+        map.zoomToExtent(vector.getDataExtent());
+        pulsate(circle);
+        firstGeolocation = false;
+        this.bind = true;
+    }
+});
+geolocate.events.register("locationfailed",this,function() {
+    OpenLayers.Console.log('Location detection failed');
+});
+document.getElementById('locate').onclick = function() {
+    vector.removeAllFeatures();
+    geolocate.deactivate();
+    geolocate.watch = false;
+    firstGeolocation = true;
+    geolocate.activate();
+};
+// document.getElementById('track').onclick = function() {
+//     vector.removeAllFeatures();
+//     geolocate.deactivate();
+//     if (this.checked) {
+//         geolocate.watch = true;
+//         firstGeolocation = true;
+//         geolocate.activate();
+//     }
+// };
+// document.getElementById('track').checked = false;
 
 }
