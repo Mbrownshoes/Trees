1774edef0f9cef1dffe1fb91b6f07b48c7eded3c
Mathew Brown <mathew.brown@utoronto.ca>
2014-07-31 10:12:07 -0400

    added unittest for form

---
+++ accounts/models.py
@@ -0,0 +1,6 @@
+# from django_browserid.auth import BrowserIDBackend
+
+# class User(BrowserIDBackend):
+#     def create_user(self, email):
+#         username = my_custom_username_algo()
+#         return self.User.objects.create_user(username, email)
--- treemap/tests/test_views.py
+++ treemap/tests/test_views.py
@@ -8,7 +8,6 @@ from treemap.views import map_page, detail
 
 from treemap.utils import get_map_form
 
-
 class HomePageTest(TestCase):
 
     def test_home_page_renders_home_template(self):
@@ -40,29 +39,30 @@ class DetailPageTest(TestCase):
     def test_displays_tree_form(self):
         tree_ = Trees.objects.create(common_nam='bla',address_po='1',address_fu='foo',objectid='1',struct_id='1',botanical_field='bar',diameter_b='1',tree_posit='1',geom='MULTIPOINT(-1.464854 52.561928)')
         response = self.client.get('/city_trees.%d/' % (tree_.id,))
-        # form_class = get_map_form(tree_.id) 
-        # geometry_field = 'geom'
-        # wkt = getattr(tree_, geometry_field)
-        # other_form = form_class({'geometry' : wkt})
-        print(get_map_form(tree_.id,))
-        self.assertIsInstance(response.context['form'], get_map_form(tree_.id) )
+        geometry_field = 'geom'
+        form_class = get_map_form(tree_.id) 
+        wkt = getattr(tree_, geometry_field)
+        form = form_class({'geometry' : wkt})
+        MapForm = form.__class__
+
+        self.assertEqual(response.context['form'].__class__.__name__,MapForm.__name__)
 
     ## probably not working cause I'm not signed in!!! response.content [in shell]
 
 
-    def test_does_display_tree_if_not_sigend_in(self):
+    def test_doesnt_display_tree_if_not_sigend_in(self):
         correct_list = Trees.objects.create(common_nam='bla',address_po='1',address_fu='foo',objectid='21',struct_id='1',botanical_field='bar',diameter_b='1',tree_posit='1',geom='MULTIPOINT(-1.464854 52.561928)')
         other_list = Trees.objects.create(common_nam='bla1',address_po='1',address_fu='foofoo',objectid='1',struct_id='1',botanical_field='bar',diameter_b='1',tree_posit='1',geom='MULTIPOINT(-1.464854 52.561928)')
 
         response = self.client.get('/city_trees.%d/' % (correct_list.id,))
-        self.assertContains(response, 'You need to be singed in')
+        self.assertNotContains(response, other_list.common_nam)
 
     def test_displays_fields_for_only_that_tree(self):
         User.objects.create(email='a@b.com')
         correct_list = Trees.objects.create(common_nam='bla',address_po='1',address_fu='foo',objectid='21',struct_id='1',botanical_field='bar',diameter_b='1',tree_posit='1',geom='MULTIPOINT(-1.464854 52.561928)')
         # self.client.login(username='mathewbrown', password='kopidulu')
         response = self.client.get('/city_trees.%d/' % (correct_list.id,))
-        print(response.content)
+        # print(response.content)
         self.assertContains(response, 'bla')
 
 
