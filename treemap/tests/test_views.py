7878cb7fe63734aa441dd8b65b3e7de8ba6a8508
Mathew Brown <mathew.brown@utoronto.ca>
2014-07-31 10:25:48 -0400

    refactored test_views. All pass

--- treemap/tests/test_views.py
+++ treemap/tests/test_views.py
@@ -26,42 +26,41 @@ class DetailPageTest(TestCase):
         login = self.client.login(username=self.username, password=self.password)
         self.assertEqual(login, True)
 
+        Trees.objects.create(common_nam='bla',address_po='1',address_fu='foo',objectid='1',struct_id='1',botanical_field='bar',diameter_b='1',tree_posit='1',geom='MULTIPOINT(-1.464854 52.561928)')
+
     def test_detail_page_uses_detail_template(self):
-        test_tree = Trees.objects.create(common_nam='bla',address_po='1',address_fu='foo',objectid='1',struct_id='1',botanical_field='bar',diameter_b='1',tree_posit='1',geom='MULTIPOINT(-1.464854 52.561928)')
+        test_tree = Trees.objects.get(common_nam='bla')
         response = self.client.get('/city_trees.%d/' % (test_tree.id,))
         self.assertTemplateUsed(response, 'treemap/detail.html')
 
     def test_passes_correct_tree_to_template(self):
-        correct_list = Trees.objects.create(common_nam='bla',address_po='1',address_fu='foo',objectid='1',struct_id='1',botanical_field='bar',diameter_b='1',tree_posit='1',geom='MULTIPOINT(-1.464854 52.561928)')
-        response = self.client.get('/city_trees.%d/' % (correct_list.id,))
-        self.assertEqual(response.context['tree'], correct_list)
+        correct_tree = Trees.objects.get(common_nam='bla')
+        response = self.client.get('/city_trees.%d/' % (correct_tree.id,))
+        self.assertEqual(response.context['tree'], correct_tree)
 
     def test_displays_tree_form(self):
-        tree_ = Trees.objects.create(common_nam='bla',address_po='1',address_fu='foo',objectid='1',struct_id='1',botanical_field='bar',diameter_b='1',tree_posit='1',geom='MULTIPOINT(-1.464854 52.561928)')
-        response = self.client.get('/city_trees.%d/' % (tree_.id,))
+        test_tree = Trees.objects.get(common_nam='bla')
+        response = self.client.get('/city_trees.%d/' % (test_tree.id,))
         geometry_field = 'geom'
-        form_class = get_map_form(tree_.id) 
-        wkt = getattr(tree_, geometry_field)
+        form_class = get_map_form(test_tree.id) 
+        wkt = getattr(test_tree, geometry_field)
         form = form_class({'geometry' : wkt})
         MapForm = form.__class__
 
         self.assertEqual(response.context['form'].__class__.__name__,MapForm.__name__)
 
-    ## probably not working cause I'm not signed in!!! response.content [in shell]
-
 
     def test_doesnt_display_tree_if_not_sigend_in(self):
-        correct_list = Trees.objects.create(common_nam='bla',address_po='1',address_fu='foo',objectid='21',struct_id='1',botanical_field='bar',diameter_b='1',tree_posit='1',geom='MULTIPOINT(-1.464854 52.561928)')
-        other_list = Trees.objects.create(common_nam='bla1',address_po='1',address_fu='foofoo',objectid='1',struct_id='1',botanical_field='bar',diameter_b='1',tree_posit='1',geom='MULTIPOINT(-1.464854 52.561928)')
-
-        response = self.client.get('/city_trees.%d/' % (correct_list.id,))
-        self.assertNotContains(response, other_list.common_nam)
+        correct_tree = Trees.objects.get(common_nam='bla')
+        other_tree = Trees.objects.create(common_nam='bla1',address_po='2',address_fu='foofoo',objectid='2',struct_id='1',botanical_field='bar',diameter_b='1',tree_posit='1',geom='MULTIPOINT(-1.464854 52.561928)')
+        response = self.client.get('/city_trees.%d/' % (correct_tree.id,))
+        self.assertNotContains(response, other_tree.common_nam)
 
     def test_displays_fields_for_only_that_tree(self):
         User.objects.create(email='a@b.com')
-        correct_list = Trees.objects.create(common_nam='bla',address_po='1',address_fu='foo',objectid='21',struct_id='1',botanical_field='bar',diameter_b='1',tree_posit='1',geom='MULTIPOINT(-1.464854 52.561928)')
+        correct_tree = Trees.objects.get(common_nam='bla')
         # self.client.login(username='mathewbrown', password='kopidulu')
-        response = self.client.get('/city_trees.%d/' % (correct_list.id,))
+        response = self.client.get('/city_trees.%d/' % (correct_tree.id,))
         # print(response.content)
         self.assertContains(response, 'bla')
 
