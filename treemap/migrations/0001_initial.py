22dbdd5a79e0ce0c235d76b2c2b69d5142593c26
Mathew Brown <mathew.brown@utoronto.ca>
2014-08-01 14:56:43 -0400

    new migration

--- treemap/migrations/0001_initial.py
+++ treemap/migrations/0001_initial.py
@@ -16,9 +16,9 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Harbord',
             fields=[
-                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
-                ('Street', models.CharField(null=True, max_length=255)),
-                ('HouseNumber', models.CharField(null=True, max_length=255)),
+                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
+                ('Street', models.CharField(max_length=255, null=True)),
+                ('HouseNumber', models.CharField(max_length=255, null=True)),
                 ('CommonSpeciesNames', models.CharField(max_length=255)),
                 ('Circumference', models.CharField(max_length=255)),
                 ('DBH', models.CharField(max_length=255)),
@@ -33,9 +33,9 @@ class Migration(migrations.Migration):
             unique_together=set([('Street', 'HouseNumber', 'CommonSpeciesNames', 'Circumference', 'DBH', 'point')]),
         ),
         migrations.CreateModel(
-            name='Trees',
+            name='HistoricalTrees',
             fields=[
-                ('id', models.AutoField(primary_key=True, verbose_name='ID', auto_created=True, serialize=False)),
+                ('id', models.IntegerField(verbose_name='ID', blank=True, auto_created=True, db_index=True)),
                 ('address_po', models.IntegerField()),
                 ('address_fu', models.CharField(max_length=131)),
                 ('objectid', models.IntegerField()),
@@ -45,7 +45,30 @@ class Migration(migrations.Migration):
                 ('diameter_b', models.IntegerField()),
                 ('tree_posit', models.IntegerField()),
                 ('geom', django.contrib.gis.db.models.fields.MultiPointField(srid=4326)),
-                ('owner', models.ForeignKey(null=True, blank=True, to=settings.AUTH_USER_MODEL)),
+                ('history_id', models.AutoField(primary_key=True, serialize=False)),
+                ('history_date', models.DateTimeField()),
+                ('history_type', models.CharField(max_length=1, choices=[('+', 'Created'), ('~', 'Changed'), ('-', 'Deleted')])),
+                ('history_user', models.ForeignKey(null=True, to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'verbose_name': 'historical trees',
+                'ordering': ('-history_date', '-history_id'),
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Trees',
+            fields=[
+                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
+                ('address_po', models.IntegerField()),
+                ('address_fu', models.CharField(max_length=131)),
+                ('objectid', models.IntegerField()),
+                ('struct_id', models.CharField(max_length=20)),
+                ('common_nam', models.CharField(max_length=254)),
+                ('botanical_field', models.CharField(max_length=254)),
+                ('diameter_b', models.IntegerField()),
+                ('tree_posit', models.IntegerField()),
+                ('geom', django.contrib.gis.db.models.fields.MultiPointField(srid=4326)),
             ],
             options={
             },
